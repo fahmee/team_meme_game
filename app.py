7cba0e3ffc22b23f397709eb50a04d23e36d00e8
Faheem Ahmad <31136780+fahmee@users.noreply.github.com>
2025-07-06 20:48:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ def get_firestore_client():
 if not st.session_state.get("host_mode", False):
     st_autorefresh(interval=10000, limit=None, key="meme_refresh")
 
+if st.session_state.get("rerun_needed", False):
+    st.session_state.rerun_needed = False
+    st.experimental_rerun()
+
 # --- FIRESTORE SETUP ---
 db = get_firestore_client()
 
@@ -230,7 +234,7 @@ if game_state and game_state.get("game_started", False):
             "current_meme": None,
             "game_started": True
         })
-        st.experimental_rerun()
+        st.session_state.rerun_needed = True
 
 else:
     if st.session_state.has_registered:
